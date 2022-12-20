a905cd4bb56dd13a789e6b87ea4fdf919d47bcda
Călin Rafa <rafa_ext@twin.one>
2022-12-20 17:07:21 +0200

    Added routines and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoutinesModule } from './routines/routines.module';
 
 @Module({
-  imports: [],
+  imports: [RoutinesModule],
   controllers: [AppController],
   providers: [AppService],
 })
