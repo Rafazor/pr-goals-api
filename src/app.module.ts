ed24c01dec0caf927818047d1b43f2e2866b8c0c
Călin Rafa <rafa_ext@twin.one>
2022-12-20 23:39:30 +0200

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RoutinesModule } from './routines/routines.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -9,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot('mongodb://localhost:27017/pr-local'),
     RoutinesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
