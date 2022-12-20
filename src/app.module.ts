5f1c1a9f12d54f05c940c9cb1ad349b871c977fa
Călin Rafa <rafa_ext@twin.one>
2022-12-20 23:42:12 +0200

    Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { RoutinesModule } from './routines/routines.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/pr-local'),
     RoutinesModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
