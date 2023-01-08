e8840fa7d83a8911249d1b1cdea2087308a9402b
Călin Rafa <rafa_ext@twin.one>
2023-01-08 21:54:21 +0200

    Added local tunnel

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { HttpExceptionFilter } from './utils/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('v1');
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
